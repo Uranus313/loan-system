be6e772eb2a8cf675c1f28614e144e874e97bfc7
Uranus313 <mehrbodmh82@gmail.com>
2024-06-17 13:05:00 +0330

    creating the sign up page

--- Loan System/src/routes/router.jsx
+++ Loan System/src/routes/router.jsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../components/Layout";
 import App from "../components/App";
+import SignUp from "../components/SignUp";
 
 const router = createBrowserRouter([
     {path: '/', element:<Layout /> ,children:[
-        {path: '', element:<App />}
+        {path: '', element:<App />},
+        {path:'signUp',element: <SignUp/>}
     ]}
 ])
 
